83a1bade359b0abf4e895c51c512f5dec1a1c096
Cameron Mathis <cmmathis98@gmail.com>
2021-11-23 13:18:15 -0500

    all crud operations now work

--- API/src/main/java/com/contactManagementSystem/utils/PasswordUtil.java
+++ API/src/main/java/com/contactManagementSystem/utils/PasswordUtil.java
@@ -2,12 +2,10 @@ package com.contactManagementSystem.utils;
 
 import java.security.SecureRandom;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class PasswordUtil {
-    @Bean
-    public String encode(String plainPassword) {
+    public static String encode(String plainPassword) {
         int strength = 10;
         BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(strength, new SecureRandom());
         String encodedPassword = bCryptPasswordEncoder.encode(plainPassword);
