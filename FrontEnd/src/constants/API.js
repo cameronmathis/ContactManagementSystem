8d66970d6746ad6fd5c6bb90bee671ac3868b9ee
Cameron Mathis <cmmathis98@gmail.com>
2021-12-09 16:19:40 -0500

    small change

--- FrontEnd/src/constants/API.js
+++ FrontEnd/src/constants/API.js
@@ -1,6 +1,6 @@
-const URL_BASE_LOCAL = "http://localhost:8080";
+const URL_BASE__LOCAL = "http://localhost:8080";
 
 const SERVER_IP = "ubu-server.com";
-const URL_BASE_SERVER = "http://" + SERVER_IP + ":8080";
+const URL_BASE__SERVER = "http://" + SERVER_IP + ":8080";
 
-export const API_URL = URL_BASE_LOCAL + "/api";
+export const API_URL = URL_BASE__LOCAL + "/api";
